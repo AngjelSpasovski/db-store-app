67703f4db5d7e653a495384646a1942753abae44
Angjel Spasovski <a.spasovski@opera-international.com>
2025-09-02 17:15:42 +0200

    production deploy

--- src/app/auth/auth.environment.ts
+++ src/app/auth/auth.environment.ts
@@ -1,7 +1,9 @@
-export const environment = {
-  production: false,
+export { environment } from '../../environments/environment';
+
+// export const environment = {
+//   production: false,
 
   
-  baseApiUrl: '/api'                                            // for DEVELOPMENT, use relative path to api
-  //baseApiUrl: 'https://web-society.kps-dev.com/api/v1'        // for PRODUCTION, use the actual API URL
-};
+//   baseApiUrl: '/api'                                            // for DEVELOPMENT, use relative path to api
+//   //baseApiUrl: 'https://web-society.kps-dev.com/api/v1'        // for PRODUCTION, use the actual API URL
+// };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseApiUrl: 'https://web-society.kps-dev.com/api/v1'
 };
